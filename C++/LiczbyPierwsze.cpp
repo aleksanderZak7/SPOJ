6b14577f85f75eeb2f3b12e652a408cc24bc2113
Aleksander Zak <olekzak7@gmail.com>
2024-04-23 13:55:53 +0200

    PRIME_T - Liczby Pierwsze

--- C++/LiczbyPierwsze.cpp
+++ C++/LiczbyPierwsze.cpp
@@ -6,21 +6,22 @@ bool is_prime(const int& number)
     if (number <= 1)
         return false;
     for (int i = 2; i <= sqrt(number); ++i) 
+    {
         if (number % i == 0)
             return false;
+    }
     return true;
 }
 
 int main() 
 {
     int n;
+    int number;
     std::cin >> n;
 
     while (n--) 
     {
-        int number;
         std::cin >> number;
-
         if (is_prime(number))
             std::cout << "TAK\n";
         else
