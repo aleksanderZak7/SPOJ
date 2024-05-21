2fd42354ab938873d86bdafdb2efef77510ebf48
Aleksander Zak <olekzak7@gmail.com>
2024-05-21 22:43:27 +0200

    PP0501A - NWD

--- Python/NWD.py
+++ Python/NWD.py
@@ -2,7 +2,6 @@ import math
 
 t = int(input()) 
 for _ in range(t):
-    numbers = input()
-    numbers = numbers.split()
+    numbers = input().split()
     x, y = map(int, numbers)
     print(math.gcd(x, y))
