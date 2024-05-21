31d5b2c6bfdb1cf5e197f4cdcc902146ec0bcd88
Aleksander Zak <olekzak7@gmail.com>
2024-05-21 22:41:50 +0200

    RNO_DOD - Proste dodawanie

--- Python/Proste_dodawanie.py
+++ Python/Proste_dodawanie.py
@@ -1,8 +1,7 @@
 t = int(input())
 for _ in range(t):
     n = int(input())
-    numbers = input() 
-    numbers = numbers.split()
+    numbers = input().split()
     output = 0
     for i in range(n):
         output += (int(numbers[i])) 
