d1c248be6c5dc0c788f84bbcbf9dd19f636e3e20
Aleksander Zak <olekzak7@gmail.com>
2024-08-08 15:00:21 +0200

    WSEGA - Wiek segmentolka

--- Python/Wiek_segmentolka.py
+++ Python/Wiek_segmentolka.py
@@ -1,9 +1,6 @@
-def calculate_segmentolek_age():
+t = int(input())
+for _ in range(t):
     data = list(map(int, input().split()))
     n = data[0]
     legs = data[1:]
     print(sum(legs) + (n - 1))
-
-t = int(input())
-for _ in range(t):
-    calculate_segmentolek_age()
