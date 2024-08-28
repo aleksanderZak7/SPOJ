2495fd87f476f04c4d6570893926ce1f99c9914c
Aleksander Zak <olekzak7@gmail.com>
2024-08-28 11:14:52 +0200

    PP0602D - ROL (k)

--- Python/ROL (k).py
+++ Python/ROL (k).py
@@ -1,5 +1,4 @@
 n, k = map(int, input().split())
 numbers = input().split()
 for i in range(n):
-    new_index = (i + k) % n
-    print(numbers[new_index], end=' ')
+    print(numbers[(i + k) % n], end=' ')
