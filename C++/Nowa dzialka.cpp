aab85763cd4c609e21e0d8597808cbf92e68dd6d
Aleksander Zak <olekzak7@gmail.com>
2024-08-28 11:01:39 +0200

    MWPZ06X - Nowa działka

---
+++ C++/Nowa dzialka.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() 
+{
+    int D, X;
+    std::cin >> D;
+    while(D--)
+    {
+        std::cin >> X;
+        std::cout << X * X << '\n';
+    }
+    return 0;
+}
