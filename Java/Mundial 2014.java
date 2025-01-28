2c14414225423a2b4843a2991293b89186477889
Aleksander Zak <olekzak007@gmail.com>
2025-01-28 22:28:54 +0100

    AL_17_01 - Mundial 2014

---
+++ Java/Mundial 2014.java
@@ -0,0 +1,8 @@
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        char A = 'A', B = 'B', C = 'C', E = 'E';
+        System.out.print("Mundial " + (C - A) + (A - A) + (B - A) + (E - A));
+    }
+}
---
+++ Python/Mundial 2014.py
@@ -0,0 +1,2 @@
+A, B, C, E = map(ord, ['A', 'B', 'C', 'E'])
+print(f"Mundial {C - A}{A - A}{B - A}{E - A}")
